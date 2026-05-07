44e8493de315407f4d09e7cbdd891fde0d06cabb
TheRudraBro <rudramraj22@gmail.com>
2026-05-08 01:28:40 +0600

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,86 @@
 // const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
+import {sql} from './config/db.js';
 
 dotenv.config();
 
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+// for parsing application/json
+// our custom simple middleware
+// app.use((req,res,next) => {
+//     console.log("Hey we hit a req, the method is ", req.method)
+//     next();
+// });
+
 const PORT = process.env.PORT || 5001;
 
-connectDB(process.env.DATABASE_URL);
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL, 
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+            )`;
+            // Decimal 10,2 
+            // Means : a fixed point number total 10 digits
+            // 2 digits after decimal points
+            // so max value is 99999999.99
 
-app.get('/', (req, res) => {
-    res.send('It is Working!');
-});
+            console.log("Database initialized successfully.");
+
+    } catch (error) {
+        console.log
+        ("Error initializing database:", error);
+        process.exit(1); // status code 1 means failure, 0 means success.
+    }
+}
+
+app.get("/", (req,res) => {
+    res.send("it's working!");
+})
+
+app.post("/api/transactions", async (req,res) => {
+   
+    try {
+        const {title, amount, category, user_id} = req.body;
+
+        if (!title || !user_id || !category || amount === undefined) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        const transaction =await sql`
+        INSERT INTO transactions (user_id, title, amount, category)
+        VALUES (${user_id}, ${title}, ${amount}, ${category})
+        RETURNING *
+        `
+console.log("Transaction created successfully:", transaction);
+        res.status(201).json(transaction[0]); 
+        // transaction is an array with one element, we want to return that element.
 
 
 
-app.listen(PORT, () => {
+
+
+    } catch (error) {
+      
+        console.log("Error creating transaction:", error);
+        res.status(500).json({message: "Internal server error"});
+        
+    }
+})
+
+
+
+initDB().then(() => {
+    app.listen(PORT, () => {
     console.log("Server is up and running on port:", PORT);
 });
+})
