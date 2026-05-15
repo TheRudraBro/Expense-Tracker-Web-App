9ff34f04f47524f33e0d94dcbac728d0755f8acf
TheRudraBro <rudramraj22@gmail.com>
2026-05-15 13:55:43 +0600

    Updated

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,14 +1,18 @@
 import { Show, useUser } from '@clerk/expo'
 import { useClerk } from '@clerk/expo'
-import { Link } from 'expo-router'
-import { Text, View, Pressable, StyleSheet } from 'react-native'
+import { Link, useRouter } from 'expo-router'
+import { Text, View, Pressable, StyleSheet, TouchableOpacity } from 'react-native'
 import { useTransactions } from '../../hooks/useTransactions'
 import { useEffect } from 'react'
 import PageLoader from '../../components/PageLoader'
 import { Image } from 'react-native'
+import { COLORS } from '../../constants/colors'
+import { styles } from '../../assets/styles/home.styles'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Page() {
   const { user } = useUser()
+  const router = useRouter()
   // const { transactions, summary, isLoading, loadData, deleteTransaction } = useTransactions(user.id)
   const { signOut } = useClerk()
 const {
@@ -47,11 +51,41 @@ source={require('../../assets/images/logo.png')}
 style={styles.headerLogo}
 resizeMode='contain'
  />
+ <View style={styles.welcomeContainer}>
+<Text style={styles.welcomeText}> 
+Welcome,
+</Text>
+<Text style={styles.usernameText}>
+  {/* Email: mrmajumder2@gmail.com so user name will be the part before @ */}
+  {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+</Text>
+
+ </View>
 </View>
 
 
 
 {/* RIGHT */}
+<View style={styles.headerRight}>
+<TouchableOpacity style={styles.addButton} onPress={() => router.push('/create')}>
+<Ionicons name="add" size={20} color="#fff" />
+<Text style={styles.addButtonText}>
+  Add
+</Text>
+</TouchableOpacity>
+ {/* LOGOUT BUTTON */}
+  <TouchableOpacity
+    style={styles.logoutButton}
+    onPress={() => signOut()}
+  >
+    <Ionicons name="log-out-outline" size={22} color={COLORS.text} />
+  </TouchableOpacity>
+
+</View>
+
+
+
+{/* Header close */}
 
 </View>
       </View>
@@ -59,27 +93,27 @@ resizeMode='contain'
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    paddingTop: 60,
-    gap: 16,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  button: {
-    backgroundColor: '#3da40a',
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: '600',
-  },
-  
-}) 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//     paddingTop: 60,
+//     gap: 16,
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//   },
+//   button: {
+//     backgroundColor: '#3da40a',
+//     paddingVertical: 12,
+//     paddingHorizontal: 24,
+//     borderRadius: 8,
+//     alignItems: 'center',
+//   },
+//   buttonText: {
+//     color: '#fff',
+//     fontWeight: '600',
+//   },
+//   welcomeText: {
+// }) 
--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -2,9 +2,9 @@
 
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
-// const API_URL = 'https://expense-tracker-api-7cex.onrender.com/api';
+const API_URL = 'https://expense-tracker-api-7cex.onrender.com/api';
 
-const API_URL = "http://localhost:5001/api";
+// const API_URL = "http://localhost:5001/api";
 
 export const useTransactions = (userId) => {
   const [transactions, setTransactions] = useState([]);
