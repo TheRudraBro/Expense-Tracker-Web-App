fc233aec9b31e9ef24771c366b78808672c76388
TheRudraBro <rudramraj22@gmail.com>
2026-05-16 00:23:20 +0600

    Updated

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,19 +1,42 @@
 
-import { Stack } from "expo-router";
-import { ClerkProvider } from "@clerk/expo";
-import  SafeScreen  from "@/components/SafeScreen";
+// import { Stack } from "expo-router";
+// import { ClerkProvider } from "@clerk/expo";
+// import  SafeScreen  from "@/components/SafeScreen";
 
-const publishableKey =
-  process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+// const publishableKey =
+//   process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+// export default function RootLayout() {
+//   return (
+//     <ClerkProvider publishableKey={publishableKey}>
+//       <SafeScreen>
+//           <Stack screenOptions={{ headerShown: false }} />
+//       </SafeScreen>
+    
+//     </ClerkProvider>
+//   );
+  
+// }
+import { Slot } from "expo-router";
+import SafeScreen from "../components/SafeScreen";
+import { ClerkProvider } from "@clerk/expo";
+import * as SecureStore from "expo-secure-store";
+import { StatusBar } from "expo-status-bar";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const tokenCache = {
+  getToken: (key) => SecureStore.getItemAsync(key),
+  saveToken: (key, value) => SecureStore.setItemAsync(key, value),
+};
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <SafeScreen>
-          <Stack screenOptions={{ headerShown: false }} />
+        <Slot />
       </SafeScreen>
-    
+      <StatusBar style="dark" />
     </ClerkProvider>
   );
-  
 }
